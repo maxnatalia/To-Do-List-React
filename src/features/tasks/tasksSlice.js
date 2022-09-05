269e563f5a3f87865ab16fee73e194105f9e79a0
Natalia Mazur-Zurek <nataliamazur1988@gmail.com>
2022-09-05 11:40:58 +0200

    created tasksSlice.js

---
+++ src/features/tasks/tasksSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState: {
+        tasks: [],
+        hideDone: false,
+    },
+    reducers: {
+        addTask: ({ tasks }, { payload: addTask }) => {
+            tasks.push(addTask);
+        },
+        toggleHideDone: state => {
+            state.hideDone = !state.hideDone;
+        },
+        toggleTaskDone: ({ tasks }, { payload: id }) => {
+            const index = tasks.findIndex(task => task.id === id);
+            tasks[index].done = !tasks[index].done
+        },
+        removeTask: ({ tasks }, { payload: id }) => {
+            const index = tasks.findIndex(task => task.id === id);
+            tasks.findIndex(task => task.id === id);
+            tasks.splice(index, 1);
+        },
+        setAllDone: ({ tasks }) => {
+            for (const task of tasks) {
+                task.done = true;
+            }
+        },
+
+    },
+});
+
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone } = tasksSlice.actions;
+export const selectTasks = state => state.tasks;
+export const selectHideDone = state => selectTasks(state).hideDone;
+export default tasksSlice.reducer;
