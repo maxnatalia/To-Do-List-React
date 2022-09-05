5ccb97bf966d53ecc3d4ef19743af039a63121ed
Natalia Mazur-Zurek <nataliamazur1988@gmail.com>
2022-09-05 11:40:09 +0200

    transferred the file Main.js

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -3,36 +3,20 @@ import TasksList from "./TasksList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
-import { Main } from "../../Main";
-import { useState } from "react";
-import { useTasks } from "../../useTasks";
+import { Main } from "./Main";
 
 function App() {
-    
-    const [hideDone, setHideDone] = useState(false);
-    const toggleHideDone = () => {
-        setHideDone(hideDone => !hideDone);
-    };
-    
-    const {
-        tasks,
-        removeTask,
-        toggleTaskDone,
-        setAllDone,
-        addNewTask,
-    } = useTasks();
-
     return (
         <Main>
             <Header title="Lista zadań" />
             <Section
                 title="Dodaj nowe zadanie"
-                body={<Form addNewTask={addNewTask} />}
+                body={<Form />}
             />
             <Section
                 title="Lista zadań"
-                body={<TasksList tasks={tasks} hideDone={hideDone} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />}
-                extraContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} setAllDone={setAllDone} />}
+                body={<TasksList />}
+                extraContent={<Buttons />}
             />
         </Main>
     );
