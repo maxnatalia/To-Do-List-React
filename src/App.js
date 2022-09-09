621786307bbd751fee791b646a2cc5ae81baaa18
maxnatalia <Nataliamazur1988@gmail.com>
2022-09-09 08:25:53 +0200

    added  Author page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import Tasks from "./features/tasks/Tasks";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import Author from "./features/author/Author";
 
-const App = () => <Tasks />;
+const App = () => (
+    <BrowserRouter>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/zadania">
+                        Zadania
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/autor">
+                        O autorze
+                    </Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                    <Tasks />
+                </Route>
+                <Route path="/autor">
+                    <Author />
+                </Route>
+            </Switch>
+        </nav>
+    </BrowserRouter>
+);
 
 export default App;
---
+++ src/features/author/Author.js
@@ -0,0 +1,33 @@
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+
+const Author = () => {
+    return (
+        <>
+            <Header title="O autorze" />
+            <Section
+                title="Natalia Mazur-Żurek"
+                body=
+                {<>
+                    <p> Cześć! Nazywam się <strong>Natalia</strong> i mieszkam od niedawna w Dąbrowie Górniczej
+                        z mężem, psem i trzema papugami🐶🦜🐦🐦.
+                    </p>
+                    <p>
+                        Ukończyłam studia - iberystykę, a dokładniej <strong>język hiszpański</strong> w biznesie i turystyce.
+                        W swojej pierwszej pracy delikatnie zetknęłam się z programowaniem - co mi się bardzo spodobało,
+                        jednak jakoś wtedy nie myślałam o tym na poważnie.
+                        Ostatnio się to zmieniło i postanowiłam, że rozpoczynam <strong>naukę programowania!</strong>
+                    </p>
+                    <p>
+                        Poza programowaniem interesuję się również aranżacją wnętrz,
+                        lubię też czasami namalować obraz,
+                        ale ostatnimi czasy (głównie przez czasy pandemiczne) stałam się... <strong>maniaczką roślinną</strong>🌱🌵🌷🌺.
+                    </p>
+
+                </>}
+            />
+        </>
+    )
+};
+
+export default Author;
