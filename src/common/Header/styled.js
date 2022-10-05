5201f0950eab86f555d6ca5bb9b981a5304661bf
maxnatalia <Nataliamazur1988@gmail.com>
2022-10-05 09:32:29 +0200

    added media queries for theme mode

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -4,6 +4,11 @@ export const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px ) {
+        display: grid;
+        grid-template-rows: 1fr;
+    }
 `;
 
 export const StyledHeader = styled.h1`
@@ -11,5 +16,15 @@ export const StyledHeader = styled.h1`
 `;
 
 export const Button = styled.button`
-   
+    border-radius: 5px;
+    width: 180px;
+    padding: 10px 15px;
+    cursor: pointer;
+    border:2px solid ${({ theme }) => theme.color.primaryColor};;
+    color: ${({ theme }) => theme.color.primaryColor};
+    background-color: ${({ theme }) => theme.color.backgroundBody};
+
+    &:hover {
+        font-weight: 600;
+    }
 `;
