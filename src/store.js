cfa229834fdc1c3a03d8b81c5585e56893995b6b
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-11 10:55:27 +0100

    added Modal to store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import tasksReducer from "./features/tasks/tasksSlice";
 import themeReducer from "./common/themeSlice";
+import modalReducer from "./features/tasks/TasksPage/Modal/modalSlice";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -9,6 +10,7 @@ const store = configureStore({
     reducer: {
         tasks: tasksReducer,
         theme: themeReducer,
+        modal: modalReducer,
     },
     middleware: [sagaMiddleware],
 });
