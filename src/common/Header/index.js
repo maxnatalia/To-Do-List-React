b1e5e2db8d7caf32c9ba6fd3cc2c7917fe216c2a
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-12 11:49:55 +0100

    added new feature - choose language

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,18 +1,32 @@
 import { Wrapper, Button, StyledHeader } from "./styled";
 import { useDispatch, useSelector } from "react-redux";
 import { selectDarkTheme, toggleTheme } from "../themeSlice";
+import { changeLanguage, selectLanguage } from "../languageSlice";
+import descriptions from "../descriptions";
 
 const Header = ({ title }) => {
     const isDarkTheme = useSelector(selectDarkTheme);
+    const language = useSelector(selectLanguage);
     const dispatch = useDispatch();
-
+    
     return (
         <Wrapper>
             <StyledHeader>
                 {title}
             </StyledHeader>
+            <div>
+                {Object.keys(descriptions).map((key) => (
+                    <Button
+                        lang
+                        key={key}
+                        value={key}
+                        onClick={() => dispatch(changeLanguage(key))}
+                    >{key}
+                    </Button>
+                ))}
+            </div>
             <Button onClick={() => dispatch(toggleTheme())}>
-                {isDarkTheme ? "Jasny motyw" : "Ciemny motyw"}
+                {isDarkTheme ? descriptions[language].themeLight : descriptions[language].themeDark}
             </Button>
         </Wrapper>
     )
