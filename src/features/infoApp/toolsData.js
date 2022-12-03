e7a3fdb5030c1ca5475aa62bf76da0ff1d83fcff
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-03 09:04:25 +0100

    deleted images from toolsData

--- src/features/infoApp/toolsData.js
+++ src/features/infoApp/toolsData.js
@@ -1,25 +1,14 @@
-import reactIcon from "./react-90.svg";
-import reduxIcon from "./redux.svg";
-import scIcon from "./styled-components-1.svg";
-
-
 export const toolsData = [
     {
         name: "React",
         tools: ["react-hooks", "components", "custom-hooks"],
-        img: reactIcon,
-        alt: "",
     },
     {
         name: "Redux",
         tools: ["redux-saga", "redux-toolkit"],
-        img: reduxIcon,
-        alt: "",
     },
     {
         name: "Styled-Components",
         tools: ["Theme", "GlobalStyles"],
-        img: scIcon,
-        alt: "",
     },
 ];
