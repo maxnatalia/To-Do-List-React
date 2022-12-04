a817b46a3b74ad418d1569db893469f4d6a7645f
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-04 09:27:42 +0100

    added languages to navigation

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,14 +1,22 @@
+import { useSelector } from "react-redux";
+import { selectLanguage } from "../languageSlice";
 import { Nav, StyledNavLink } from "./styled";
+import descriptions from "../descriptions";
 
 const Navigation = () => {
+    const language = useSelector(selectLanguage);
+
     return (
         <Nav>
             <StyledNavLink to="/zadania">
-                Zadania
+                {descriptions[language].tasksPageTitle}
             </StyledNavLink>
 
             <StyledNavLink to="/autor">
-                O autorze
+                {descriptions[language].authorPageTitle}
+            </StyledNavLink>
+            <StyledNavLink to="/info">
+                O aplikacji
             </StyledNavLink>
         </Nav>
     )
