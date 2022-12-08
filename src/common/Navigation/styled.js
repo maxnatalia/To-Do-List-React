034ee92f0b4f96287f98dd362b4daa772f7b1589
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-08 10:40:18 +0100

    added styles to mobile menu navigation

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export const StyledNavLink = styled(NavLink)`
     color: ${({ theme }) => theme.color.backgroundColorsMain};
@@ -21,9 +22,74 @@ export const Nav = styled.nav`
     background-color: ${({ theme }) => theme.color.primaryColor};
     display: flex;
     justify-content: center;
+    gap: 20px;
     width: 100%;
-    padding: 10px;
+    padding: 20px;
     position: fixed;
     height: 60px;
-   
+    text-align: center;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px ) {
+        padding: 0;
+    }
+`;
+
+export const NavMenu = styled.div`
+    flex-grow: 2;
+    
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px ) {
+        opacity: ${({ show }) => show ? "1" : "0"};
+        visibility: ${({ show }) => show ? "visible" : "hidden"};
+        background-color: ${({ theme }) => theme.color.primaryColor};
+        width: 100%;
+        height: 100vh;
+        display: grid;
+        grid-template-columns: 1fr;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+export const MobileMenu = styled.div`
+`;
+
+export const Container = styled.div`
+    display: flex;
+    width: 100%;
+`;
+
+export const MenuBars = styled(FaBars)`
+    display: none;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px ) {
+        display: flex;
+        color: ${({ theme }) => theme.color.backgroundColorsMain};
+        justify-self: flex-end;
+        align-self: center;
+        margin: 15px;
+        font-size: 20px;
+        position: absolute;
+        right: 20px;
+        top: 10px;
+        cursor: pointer;
+        z-index: 50;
+    }
+`;
+
+export const MenuClose = styled(FaTimes)`
+        display: none;
+
+        @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px ) {
+            display: flex;
+            color: ${({ theme }) => theme.color.backgroundColorsMain};
+            justify-self: flex-end;
+            align-self: center;
+            margin: 15px;
+            font-size: 20px;
+            position: absolute;
+            right: 20px;
+            top: 10px;
+            cursor: pointer;
+            z-index: 50;
+        }
 `;
