f13ccd6c3d93c79d04b27e587ad6225e1e3a38b1
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-08 10:39:51 +0100

    added show navigation for mobile

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,23 +1,36 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import { selectLanguage } from "../languageSlice";
-import { Nav, StyledNavLink } from "./styled";
+import { Nav, StyledNavLink, MenuBars, MenuClose, NavMenu, MobileMenu, Container } from "./styled";
 import descriptions from "../descriptions";
 
 const Navigation = () => {
+    const [show, setShow] = useState(false);
     const language = useSelector(selectLanguage);
+
+    const handleClick = () => {
+        setShow(!show)
+    };
 
     return (
         <Nav>
-            <StyledNavLink to="/zadania">
-                {descriptions[language].tasksPageTitle}
-            </StyledNavLink>
+            <Container>
+                <MobileMenu onClick={handleClick}>
+                    {show ? <MenuClose /> : <MenuBars />}
+                </MobileMenu>
+                <NavMenu show={show}>
+                    <StyledNavLink to="/zadania">
+                        {descriptions[language].tasksPageTitle}
+                    </StyledNavLink>
 
-            <StyledNavLink to="/autor">
-                {descriptions[language].authorPageTitle}
-            </StyledNavLink>
-            <StyledNavLink to="/info">
-                O aplikacji
-            </StyledNavLink>
+                    <StyledNavLink to="/autor">
+                        {descriptions[language].authorPageTitle}
+                    </StyledNavLink>
+                    <StyledNavLink to="/info">
+                        {descriptions[language].titleInfoApp}
+                    </StyledNavLink>
+                </NavMenu>
+            </Container>
         </Nav>
     )
 }
