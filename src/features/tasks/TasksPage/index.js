b9c1b36677c13863cf7aaa0a0c20d3d28a34c5e7
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-11 09:45:20 +0100

    added langugages to TasksPage

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -4,37 +4,44 @@ import Buttons from "./Buttons";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import { Main } from "../../../common/Main";
-import ExtraButton from "./ExtraButton";
+import ExtraButton from "../ExtraButton";
 import Search from "./Search";
 import Nav from "../../../common/Navigation";
 import { useSelector } from "react-redux";
-import { selectIsOpen } from "./Modal/modalSlice";
-import Modal from "./Modal/index";
+import { selectLanguage } from "../../../common/languageSlice";
+import descriptions from "../../../common/descriptions";
+import Footer from "../../../common/Footer";
+import { useEffect } from "react";
 
 function TasksPage() {
-    const isOpen = useSelector(selectIsOpen);
+
+    const language = useSelector(selectLanguage);
+
+    useEffect(() => {
+        document.title = descriptions[language].headerTitle;
+    });
 
     return (
         <>
             <Nav />
             <Main>
-                {isOpen && <Modal />}
-                <Header title="Lista zadań" />
+                <Header title={descriptions[language].headerTitle} />
                 <Section
-                    title="Dodaj nowe zadanie"
+                    title={descriptions[language].sectionTitle}
                     extraContent={<ExtraButton />}
                     body={<Form />}
                 />
                 <Section
-                    title="Wyszukiwarka"
+                    title={descriptions[language].searchSectionTitle}
                     body={<Search />}
                 />
                 <Section
-                    title="Lista zadań"
+                    title={descriptions[language].tasksSectionTitle}
                     body={<TasksList />}
                     extraContent={<Buttons />}
                 />
             </Main>
+            <Footer />
         </>
     );
 };
