4a1930f3ed7fafbc3151e3753f11422f1b020d5a
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-11 09:45:58 +0100

    added langugages and modal option removeAll

--- src/features/tasks/TasksPage/Buttons/index.js
+++ src/features/tasks/TasksPage/Buttons/index.js
@@ -1,33 +1,48 @@
 import { Button, Container } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
-import { toggleHideDone, setAllDone, selectHideDone, selectIsEveryTaskDone, selectAreTasksEmpty } from "../../tasksSlice";
-import { openModal } from "../Modal/modalSlice";
+import { toggleHideDone, setAllDone, selectHideDone, selectIsEveryTaskDone, selectAreTasksEmpty, removeAllTasks } from "../../tasksSlice";
+import { openModalAll, selectIsOpenAll, closeModal } from "../Modal/modalSlice";
+import descriptions from "../../../../common/descriptions";
+import { selectLanguage } from "../../../../common/languageSlice";
+import Modal from "../Modal";
+import { ConfirmButton } from "../Modal/styled";
 
 const Buttons = () => {
     const hideDone = useSelector(selectHideDone);
     const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
     const areTasksEmpty = useSelector(selectAreTasksEmpty);
-
+    const language = useSelector(selectLanguage);
+    const isOpenAll = useSelector(selectIsOpenAll);
     const dispatch = useDispatch();
-    
+
     return (
         <Container>
-            { areTasksEmpty && (
+            {areTasksEmpty && (
                 <>
                     <Button onClick={() => dispatch(toggleHideDone())}>
-                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                        {hideDone ? descriptions[language].toggleButtonInnerTextHidden : descriptions[language].toggleButtonInnerTextVisible}
                     </Button>
                     <Button
-                        onClick={() => dispatch(setAllDone())} 
+                        onClick={() => dispatch(setAllDone())}
                         disabled={isEveryTaskDone}
                     >
-                        Ukończ wszystkie
+                        {descriptions[language].setDoneButtonInnerText}
                     </Button>
                     <Button
-                        onClick={() => dispatch(openModal())}
+                        onClick={() => dispatch(openModalAll())}
                     >
-                        Usuń wszystkie
+                        {descriptions[language].removeAllInnerText}
                     </Button>
+                    {isOpenAll && <Modal
+                        titleModal={descriptions[language].modalContent}
+                        modalClick={
+                            <ConfirmButton onClick={() => {
+                                dispatch(removeAllTasks())
+                                dispatch(closeModal());
+                            }}
+                            >{descriptions[language].modalApproved}
+                            </ConfirmButton>} />
+                    }
                 </>
             )}
         </Container>
