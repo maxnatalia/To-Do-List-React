09a5d60c8fb32fbcde365ac9c1a581e951b1cccd
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-11 09:49:19 +0100

    created modalSlice.js

--- src/features/tasks/TasksPage/Modal/modalSlice.js
+++ src/features/tasks/TasksPage/Modal/modalSlice.js
@@ -4,17 +4,25 @@ const modalSlice = createSlice({
     name: "modal",
     initialState: {
         isOpen: false,
+        isOpenAll: false,
     },
     reducers: {
+        openModalAll: (state, _) => {
+            state.isOpenAll = true;
+            state.isOpen = false;
+        },
         openModal: (state, _) => {
             state.isOpen = true;
+            state.isOpenAll = false;
         },
         closeModal: (state, _) => {
             state.isOpen = false;
+            state.isOpenAll = false;
         },
     },
 });
 
-export const { openModal, closeModal} = modalSlice.actions;
+export const { openModal, closeModal, openModalAll} = modalSlice.actions;
 export const selectIsOpen = state => state.modal.isOpen;
+export const selectIsOpenAll = state => state.modal.isOpenAll;
 export default modalSlice.reducer;
