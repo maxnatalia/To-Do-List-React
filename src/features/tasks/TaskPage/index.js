d514272bc1d8345d0a777cc4655d419de09768e2
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-11 09:44:57 +0100

    added languages to TaskPage

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -4,29 +4,39 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getTaskById } from "../tasksSlice";
 import Nav from "../../../common/Navigation";
-import {Main} from "../../../common/Main";
+import { Main } from "../../../common/Main";
 import { StyledLinkTask } from "../TasksPage/TasksList/styled";
+import { selectLanguage } from "../../../common/languageSlice";
+import descriptions from "../../../common/descriptions";
+import Footer from "../../../common/Footer";
 
 const TaskPage = () => {
-
+    const language = useSelector(selectLanguage);
     const { id } = useParams();
     const task = useSelector(state => getTaskById(state, id));
+
     return (
         <>
             <Nav />
             <Main>
-                <Header title="Szczegóły zadania" />
+                <Header title={descriptions[language].taskPageTitle} />
                 <Section
-                    title={task ? task.content : "Niestety, nie znaleziono zadania😒"}
+                    title={task ? task.content : descriptions[language].taskStatusNotFound}
                     extraContent={""}
                     body={
                         task ?
                             <>
-                                <strong>Ukończono:</strong>
-                                {task.done ? " Tak" : " Nie"}
-                            </> : <StyledLinkTask to="/zadania">Powrót do Strony Głównej</StyledLinkTask>}
+                                <strong>{descriptions[language].taskStatusLabel}</strong>
+                                {task.done
+                                    ?
+                                    descriptions[language].taskStatusDone
+                                    :
+                                    descriptions[language].taskStatusUndone
+                                }
+                            </> : <StyledLinkTask to="/zadania">{descriptions[language].pageLink}</StyledLinkTask>}
                 />
             </Main>
+            <Footer />
         </>
     )
 };
