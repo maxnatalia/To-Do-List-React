9a5e0c9df6007dfe81c0f7773b391431493fdbe2
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-14 10:45:49 +0100

    added languageSaga to rootSaga.js

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import { tasksSaga } from "./features/tasks/tasksSaga";
 import themeSaga from "../src/common/themeSaga";
+import languageSaga from "./common/languageSaga";
 
 export default function* rootSaga() {
     yield all([
         tasksSaga(),
         themeSaga(),
+        languageSaga(),
     ]);
 }
